708f017c259273e454f2109e1952a942952d7e11
lexi <alexiacabanel1@gmail.com>
2024-02-08 16:45:42 +0100

    premier et dernier ajout :)

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,26 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+City.destroy_all
+Dogsitter.destroy_all
+Dog.destroy_all
+Stroll.destroy_all
+require 'faker'
+
+cities = []
+10.times do
+  city = City.create(name: Faker::Address.city)
+  cities << city
+end
+
+10.times do
+    dogsitter = Dogsitter.create!(name: Faker::Name.first_name, city_id: cities[rand(0..9)].id)
+end
+
+10.times do 
+    dog = Dog.create!(name: Faker::Name.first_name, city_id: cities[rand(0..9)].id)
+end
+
+30.times do
+    strolls = Stroll.create(date: Faker::Time.between_dates(from: Date.today, to: Date.today + 365, period: :day), dog_id: Dog.all.shuffle.last.id, dogsitter_id: Dogsitter.all.shuffle.last.id)
+end
